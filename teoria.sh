829b07fa054546e643af2ac8ffccfa1daeeed689
Matheus Andrade <matheus.andrade@objective.com.br>
2021-01-07 00:14:31 -0300

    First module

---
+++ teoria.sh
@@ -0,0 +1,13 @@
+#### Declaração de Variáveis ####
+#Se colocar aspas é string, senão é um valor qualquer
+#Por via de regra, na chamada da variável, se string..
+#usar echo "$NOME", se n usar aspas nao vai manter a formatação
+
+NOME="string"
+NOME=valor
+
+
+#### Permissão de execução no script ####
+chmod +x file.sh
+
+#Variáveis reservadas
